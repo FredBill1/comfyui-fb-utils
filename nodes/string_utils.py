c51cd101a4b42971aaadf00893f56aa7713191fb
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2024-04-25 18:19:35 +0100

    update

--- nodes/string_utils.py
+++ nodes/string_utils.py
@@ -69,3 +69,59 @@ class FBStringJoin:
         if not delimiter_as_raw_str:
             delimiter = codecs.decode(delimiter, "unicode_escape")
         return (delimiter.join(value),)
+
+
+@register_node("FB String Replace")
+class FBStringReplace:
+    CATEGORY = "FredBill1"
+    INPUT_TYPES = lambda: {
+        "required": {
+            "value": ("STRING", {"forceInput": True}),
+            "olds": ("STRING", {"default": "", "multiline": True, "dynamicPrompts": False}),
+            "news": ("STRING", {"default": "", "multiline": True, "dynamicPrompts": False}),
+            "recursive": ("BOOLEAN", {"default": True}),
+        }
+    }
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "execute"
+
+    def execute(self, value: str, olds: str, news: str, recursive: bool) -> tuple[str]:
+        olds = [line for line in olds.split("\n")]
+        news = [line for line in news.split("\n")]
+        assert len(olds) == len(news), "The number of old and new strings must be the same."
+        while True:
+            old_value = value
+            for old, new in zip(olds, news):
+                value = value.replace(old, new)
+            if not (recursive and old_value != value):
+                break
+        return (value,)
+
+
+@register_node("FB String Strip")
+class FBStringStrip:
+    CATEGORY = "FredBill1"
+    INPUT_TYPES = lambda: {
+        "required": {
+            "value": ("STRING", {"forceInput": True}),
+            "chars": ("STRING", {"default": ""}),
+            "chars_as_raw_str": ("BOOLEAN", {"default": False}),
+            "left": ("BOOLEAN", {"default": True}),
+            "right": ("BOOLEAN", {"default": True}),
+        }
+    }
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "execute"
+
+    def execute(self, value: str, chars: str, chars_as_raw_str: bool, left: bool, right: bool) -> tuple[str]:
+        if chars and not chars_as_raw_str:
+            chars = codecs.decode(chars, "unicode_escape")
+        if not chars:
+            chars = None
+        if left and right:
+            value = value.strip(chars)
+        elif left:
+            value = value.lstrip(chars)
+        elif right:
+            value = value.rstrip(chars)
+        return (value,)
